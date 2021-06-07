15f526340f3343baac0c88f5c46d642c872280bc
Connell McCarthy <connellmccarthy@gmail.com>
2021-06-07 17:46:56 -0400

    Better strings, margin loop fix, max width, cursor, projects hover, contact marquee

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,42 @@
-const cursor = document.querySelector('#cursor');
+var mouseX = window.innerWidth/2,
+    mouseY = window.innerHeight/2;
 
 document.querySelectorAll('a').forEach(function(x) {
+  const cursorEl = document.querySelector('#cursor');
   x.addEventListener('mouseover', function() {
-    cursor.classList.add('active');
+    cursorEl.classList.add('active');
   });
   x.addEventListener('mouseleave', function() {
-    cursor.classList.remove('active');
-  })
+    cursorEl.classList.remove('active');
+  });
 });
 
-const moveCursor = (e) =>{
-  cursor.style.left = e.pageX + 'px';
-  cursor.style.top = e.pageY + 'px';
+var cursor = {
+  el: document.querySelector('#cursor'),
+  x: window.innerWidth/2,
+  y: window.innerHeight/2,
+  w: 48,
+  h: 48,
+  update: function() {
+    l = this.x - this.w/2;
+    t = this.y - this.h/2;
+    this.el.style.transform = `translate3d(${l}px,${t}px,0)`;
+  }
 }
-document.addEventListener('mousemove', moveCursor);
+
+document.addEventListener('mousemove', function(e) {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+setInterval(move,1000/60);
+
+function move() {
+  cursor.x = lerp (cursor.x, mouseX, 0.1);
+  cursor.y = lerp (cursor.y, mouseY, 0.1);
+  cursor.update();
+}
+
+function lerp (start, end, amt) {
+  return (1-amt)*start+amt*end;
+}
