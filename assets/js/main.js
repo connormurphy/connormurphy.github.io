22034338e1cbcded06b076e38803e73207a20a0f
Connell McCarthy <connellmccarthy@gmail.com>
2021-12-06 17:46:26 -0500

    The final tweaks baby

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,8 @@
 window.onpageshow = () => {
   if (event.persisted) {
-      window.location.reload() 
+    window.location.reload() 
   }
 }
-
-var mouseX = window.innerWidth/2,
-    mouseY = window.innerHeight/2;
-
 function is_local(link) {
   if (link.host == window.location.host) {
     return true;
@@ -14,7 +10,6 @@ function is_local(link) {
     return false;
   }
 }
-
 document.querySelectorAll('a').forEach(function(x) {
   const cursorEl = document.querySelector('#cursor');
   x.addEventListener('mouseover', function() {
@@ -25,7 +20,7 @@ document.querySelectorAll('a').forEach(function(x) {
   });
   x.addEventListener('click', function() {
     console.log(x.host);
-    if (is_local(x)) {
+    if (is_local(x) && x.classList.getAttribute('target') != '_blank') {
       event.preventDefault();
       document.querySelector('.loading-screen').classList.toggle('animated');
       setTimeout(function() {
@@ -35,35 +30,50 @@ document.querySelectorAll('a').forEach(function(x) {
   });
 });
 
-var cursor = {
-  el: document.querySelector('#cursor'),
-  x: window.innerWidth/2,
-  y: window.innerHeight/2,
-  w: 48,
-  h: 48,
-  update: function() {
-    l = this.x - this.w/2;
-    t = this.y - this.h/2;
-    this.el.style.transform = `translate3d(${l}px,${t}px,0)`;
+//Mouse cursor
+
+const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+const iPad = !!(navigator.userAgent.match(/(iPad)/)
+  || (navigator.platform === "MacIntel" && typeof navigator.standalone !== "undefined"));
+
+var showCursor = true;
+
+if (iOS || iPad) {
+  document.getElementById('cursor').remove();
+  showCursor = false;
+}
+
+if (showCursor) {
+  var mouseX = window.innerWidth/2,
+    mouseY = window.innerHeight/2;
+  var cursor = {
+    el: document.querySelector('#cursor'),
+    x: window.innerWidth/2,
+    y: window.innerHeight/2,
+    w: 48,
+    h: 48,
+    update: function() {
+      l = this.x - this.w/2;
+      t = this.y - this.h/2;
+      this.el.style.transform = `translate3d(${l}px,${t}px,0)`;
+    }
+  }
+  document.addEventListener('mousemove', function(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+  setInterval(move,1000/60);
+  function move() {
+    cursor.x = lerp (cursor.x, mouseX, 0.1);
+    cursor.y = lerp (cursor.y, mouseY, 0.1);
+    cursor.update();
+  }
+  function lerp (start, end, amt) {
+    return (1-amt)*start+amt*end;
   }
 }
 
-document.addEventListener('mousemove', function(e) {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-});
-
-setInterval(move,1000/60);
-
-function move() {
-  cursor.x = lerp (cursor.x, mouseX, 0.1);
-  cursor.y = lerp (cursor.y, mouseY, 0.1);
-  cursor.update();
-}
-
-function lerp (start, end, amt) {
-  return (1-amt)*start+amt*end;
-}
+//Project slideshow
 
 if (document.querySelector('.project')) {
   document.querySelectorAll('.project').forEach(function(x) {
